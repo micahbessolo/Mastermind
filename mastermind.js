8dd209e13590350f8258623e59fa58176b1e47cd
Micah Bessolo <83624003+micahbessolo@users.noreply.github.com>
2022-11-25 17:08:02 -0800

    table improvements

--- mastermind.js
+++ mastermind.js
@@ -1,5 +1,14 @@
 let guessAmount = 10;
 let guess = [];
-const row = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
-let red; black; white; yellow; blue; green;
+const gridIDs = ['a1', 'a2', 'a3', 'a4', 'b1', 'b2', 'b3', 'b4', 'c1', 'c2', 'c3', 'c4', 'd1', 'd2', 'd3', 'd4', 'e1', 'e2', 'e3', 'e4', 'f1', 'f2', 'f3', 'f4', 'g1', 'g2', 'g3', 'g4', 'h1', 'h2', 'h3', 'h4', 'i1', 'i2', 'i3', 'i4', 'j1', 'j2', 'j3', 'j4'];
+// let red; black; white; yellow; blue; green;
 
+// create the classes
+function createClasses() {
+    for (let i = 0; i < gridID.length; i++) {
+        // document.getElementById(gridIDs[i]).style.
+    }
+    return classArray;
+}
+
+// console.log(createClasses());
