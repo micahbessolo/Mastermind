9c8f8077806ea97c02c0196df0d9bac79f62b8ed
Micah Bessolo <83624003+micahbessolo@users.noreply.github.com>
2022-12-01 16:28:44 -0800

    made function to get an array of four random colors

--- mastermind.js
+++ mastermind.js
@@ -1,8 +1,23 @@
 let guessAmount = 10;
 let guess = [];
 const gridIDs = ['a1', 'a2', 'a3', 'a4', 'b1', 'b2', 'b3', 'b4', 'c1', 'c2', 'c3', 'c4', 'd1', 'd2', 'd3', 'd4', 'e1', 'e2', 'e3', 'e4', 'f1', 'f2', 'f3', 'f4', 'g1', 'g2', 'g3', 'g4', 'h1', 'h2', 'h3', 'h4', 'i1', 'i2', 'i3', 'i4', 'j1', 'j2', 'j3', 'j4'];
-// let red; black; white; yellow; blue; green;
+const colors = ['red', 'black', 'white', 'yellow', 'blue', 'green'];
 
+// gets four random colors
+function fourRandomColors() {
+    let correctColors = [];
+    let sortedColors = colors;
+    for (let i = 0; i < 4; i++) {
+        let randomColor = sortedColors[Math.floor(Math.random() * sortedColors.length)];
+        correctColors.push(randomColor);
+        sortedColors = sortedColors.filter(function(item) { // removes the random card from the sorted deck
+            return item !== randomColor;
+        });
+    }
+    return correctColors;
+}
+console.log(fourRandomColors())
+;
 // create the classes
 function createClasses() {
     for (let i = 0; i < gridID.length; i++) {
